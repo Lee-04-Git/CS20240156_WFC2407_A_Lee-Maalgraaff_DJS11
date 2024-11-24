8f1b16e3c93c75066475ee997e525a12d0a23e7d
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-24 19:16:01 +0200

    used useState to initialize state for podcasts data, loading, and error

---
+++ src/components/PodcastHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayPodcastData = () => {
+    const [podcastsData, setPodcastsData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
