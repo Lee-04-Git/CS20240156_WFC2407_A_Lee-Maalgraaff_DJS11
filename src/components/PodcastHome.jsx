2ec778008c36b2bdec4078213e52777c9ce40d6b
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-24 19:22:33 +0200

    used useEffect to fetch podcast data, handle loading and errors

--- src/components/PodcastHome.jsx
+++ src/components/PodcastHome.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const DisplayPodcastData = () => {
     const [podcastsData, setPodcastsData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    useEffect(() => {
+    const fetchPodcasts = async () => {
+      try {
+        const response = await fetch("https://podcast-api.netlify.app");
+        const data = await response.json();
+
+      } catch (err) {
+        setError("Failed to fetch data");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPodcasts();
+  }, []);
+
+
 }
