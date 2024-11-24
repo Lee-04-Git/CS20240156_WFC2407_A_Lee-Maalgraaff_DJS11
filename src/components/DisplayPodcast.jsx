25d351c96cbf26ae4f26e027572988f4c409e439
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-24 21:01:24 +0200

    set up routing with React Router for podcast home and details

---
+++ src/components/DisplayPodcast.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Ensure this is imported if using React Router
+import DisplayPodcastData from './PodcastHome';
+import './DisplayPodcast.css'; // Import the CSS file
+import DisplayPodcastDetails from './PodcastDetails';
