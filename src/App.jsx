e28256b4391296a4b96095a8d988b55e4ac2dd48
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-25 12:16:05 +0200

    rename and update component import from PodcastHome to DisplayPodcast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import DisplayPodcastData from './components/PodcastHome'
+import DisplayPodcast from './components/DisplayPodcast';
 
 function App() {
 
   return (
     <div>
-      <DisplayPodcastData />
+      <DisplayPodcast />
     </div>
   )
 }
