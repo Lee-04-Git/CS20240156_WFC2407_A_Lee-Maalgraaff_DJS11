21008eefa165587b601da39c8e8b62e6601483c3
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-25 13:34:29 +0200

    imported Navbar into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import DisplayPodcast from './components/DisplayPodcast';
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <DisplayPodcast />
     </div>
   )
