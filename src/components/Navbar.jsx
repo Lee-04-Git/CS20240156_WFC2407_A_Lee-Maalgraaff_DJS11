5bf8a6be7c07bb493bc2cfe76367823f7cc7838d
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-26 13:40:44 +0200

    refactor Navbar to include search input with state management

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.css";
+import searchIcon from "../assets/search.png";
 
-const Navbar = () => {
+const Navbar = ({search, setSearch}) => {
   return (
     <div>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <div className="navbar-search-bar">
-                    <img 
-                    src='src/assets/search.png'
-                    alt="Search-icon"
-                    className='search-icon'
-                    />
-                    <input 
-                    type="search"
-                    className='search-input'
-                    placeholder="Search podcast..."
-                    aria-label="Search"
-                    />
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-search-bar">
+            <img src={searchIcon} alt="Search-icon" className="search-icon" />
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search podcast..."
+              aria-label="Search"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
