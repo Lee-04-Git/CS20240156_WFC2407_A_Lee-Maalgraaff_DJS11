aba4cb18c91ff7151799eabe6de7da9baaf7885b
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-26 13:41:15 +0200

    removed Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import DisplayPodcast from './components/DisplayPodcast';
-import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
-      <Navbar />
       <DisplayPodcast />
     </div>
   )
