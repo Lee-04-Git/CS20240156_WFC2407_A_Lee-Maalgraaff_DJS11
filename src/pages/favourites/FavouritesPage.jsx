a9a3785fb110d3b1835e75ab9588e6afd3b9292c
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-28 11:42:51 +0200

    create FavouritesPage component to display favourites

---
+++ src/pages/favourites/FavouritesPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/FavouritesPage.js
+import React from "react";
+import Favourites from "../../components/Favourites";
+
+const FavouritesPage = () => {
+  return (
+    <div>
+      <Favourites />
+    </div>
+  );
+};
+
+export default FavouritesPage;
