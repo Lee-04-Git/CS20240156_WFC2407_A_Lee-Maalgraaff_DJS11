3375c8566ba0836ebb904ad057c29ad89fc3a7de
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-28 11:41:12 +0200

    added routing for Favourites page in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
-import DisplayPodcast from './components/DisplayPodcast';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import DisplayPodcastDetails from "./pages/viewPodcast/PodcastDetails";
+import DisplayPodcastData from "./pages/home/PodcastHome";
+import FavouritesPage from "./pages/favourites/FavouritesPage";
 
 function App() {
-
   return (
-    <div>
-      <DisplayPodcast />
+    <div className="podcast-display-container">
+      <Routes>
+        <Route path="/" element={<DisplayPodcastData />} />
+        <Route path="/podcast/:id" element={<DisplayPodcastDetails />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
